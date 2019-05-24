ac2c4fb6f99192bcf54d78d0bed620489a41c759
Aksel Stadler Kjetså <41793839+askasp@users.noreply.github.com>
2019-05-24 21:51:32 +0200

    Main character page (#1)
    * Add draft of main page view
    
    * Add Grid for stats and attribute cards

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Appbar from './AppBarDark'
+import { ThemeProvider } from '@material-ui/styles';
+import {createMuiTheme} from '@material-ui/core/styles'
+import NestedList from './NestedList'
+import {useRoutes} from 'hookrouter';
+
+
+const theme = createMuiTheme({
+    palette:{
+        collapsible:"#1e1e1e",
+        background:"#121212",
+        text:{
+            primary:"rgba(255,255,255,0.87)",
+            secondary:"rgba(255,255,255,0.60)",
+            disabled:"rgba(255,255,255,0.38)",
+            
+        }
+    }
+}
+)
+
+const routes = {
+    //'/': () => <div></div> ,
+    '/character': () => <NestedList />,
+    //'/products': () => <ProductOverview />,
+    //'/products/:id': ({id}) => <ProductDetails id={id} />
+};
+
+//<NestedList/>
 
 function App() {
+    const routeResult = useRoutes(routes);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ThemeProvider theme={theme}>
+    <div className="App" style={{backgroundColor:"#121212"}}>
+<Appbar/>
+<NestedList/>
+
     </div>
+</ThemeProvider>
   );
 }
 
 export default App;
+
+const MyApp = () => {
+    const routeResult = useRoutes(routes);
+
+    //return routeResult || <NotFoundPage />;
+}
